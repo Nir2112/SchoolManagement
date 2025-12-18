75bc1b6621abdd751eb15e6fe9d97676d37a436d
nir hadari <n.hada4@college.labs>
2025-12-18 12:30:46 +0200

    login & register: axios request, third commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,37 @@
+import { useState,useEffect,useRef, use } from "react" 
+import axios from "axios"
 
 const Login =() =>{
+    const [id,setId] = useState("")
+    const [pass,setPass] = useState("")
+
+    const checkLogin=()=>{
+        axios.get(`http://10.100.11.15:3000/login`,{
+            params:{
+                id: id,
+                pass: pass
+            }
+        })
+        .then((res)=>{
+            alert("seccessful Login");         
+        })
+        .catch((err)=>{
+            console.log(err);
+            alert("error")
+
+            
+        })
+    }
+
+
     return(<div>
         <h1>Login page</h1>
         <div>
-            <input type="text" placeholder="id"/>
-            <input type="text" placeholder="password"/>
+            <input type="text" placeholder="id" onChange={(e) => setId(e.target.value)}/>
+            <input type="text" placeholder="password" onChange={(e) => setPass(e.target.value)}/>
         </div>
         <div>
-            <button>Login</button>
+            <button onClick={checkLogin}>Login</button>
             <button>Register</button>
         </div>
     </div>)
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react"
+import axios from "axios"
+
 const Register =() =>{
+    const [student, setStudent] = useState([])
+    
+    const addField=(field,value)=>{
+        setStudent({
+            ...student,
+            [field]:value
+        })
+    }
+
+    const addStudent=()=>{
+        axios.post(`http://10.100.11.15:3000/register`,student)
+        .then((res)=>{
+            alert("seccessful Register");  
+        })
+        .catch((err)=>{
+            console.log(err);  
+            alert("error")       
+        })
+    }
+
     return(<div>
         <h1>Register page</h1>
-        <div>
-            <input type="text" placeholder="id"/>
-            <input type="text" placeholder="name"/>
-            <input type="text" placeholder="pass"/>
-            <input type="text" placeholder="email"/>
-            <input type="text" placeholder="birthday"/>
-            <input type="text" placeholder="field of study"/>
+        <div style={{display:'flex',flexDirection:'column'}}>
+            <input type="text" placeholder="id" onChange={(e) => addField("id",e.target.value)}/>
+            <input type="text" placeholder="name" onChange={(e) => addField("name",e.target.value)}/>
+            <input type="text" placeholder="password" onChange={(e) => addField("password",e.target.value)}/>
+            <input type="text" placeholder="email" onChange={(e) => addField("email",e.target.value)}/>
+            <input type="text" placeholder="phone" onChange={(e) => addField("phone",e.target.value)}/>
+            <input type="text" placeholder="birthday" onChange={(e) => addField("birthday",e.target.value)}/>
+            <input type="text" placeholder="study" onChange={(e) => addField("study",e.target.value)}/>
         </div>
          <div>
             <button>Login</button>
-            <button>Register</button>
+            <button onClick={addStudent}>Register</button>
         </div>
     </div>)
 } 
